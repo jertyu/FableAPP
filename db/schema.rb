bfc0be2153319d8291f393f296b25e481c428a19
Kirtus Justus <kirtusjustus@outlook.com>
2016-11-23 17:47:19 -0800

    Owner variable

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161120162511) do
+ActiveRecord::Schema.define(version: 20161124012049) do
 
   create_table "admins", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -109,6 +109,7 @@ ActiveRecord::Schema.define(version: 20161120162511) do
     t.integer  "avatar_file_size"
     t.datetime "avatar_updated_at"
     t.boolean  "admin",                  default: false
+    t.boolean  "owner",                  default: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.index ["username"], name: "index_users_on_username", unique: true
